420ff00ebc3726a2d659ae78c477a3c9acb8c6ba
LilAtza <ysaacnoe.correa@unmsm.edu.pe>
2024-09-20 22:47:49 -0500

    ✨ (login): implement base login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import './login.scss';
+import Image from "next/image";
+import OcTheme from "@/app/shared/components/oc-theme";
+
+export default function Login() {
+  const loginImage = 'https://i.postimg.cc/rwvyck9n/login.png';
+  const logoImage = 'https://i.postimg.cc/Y9r5BnTD/YachayL.png';
+  return (
+    <section className="login">
+      <aside className='login-theme oc-padding-small'>
+        <OcTheme/>
+      </aside>
+      <section className="login-main">
+        <div className="login-form oc-padding-medium">
+          <div className="login-logo">
+            <Image src={logoImage} alt="login-logo" width={64} height={64}/>
+          </div>
+        </div>
+      </section>
+      <div className="login-background oc-surface-container-high">
+        <Image src={loginImage} alt="login-background" layout="responsive" width={100} height={100}/>
+      </div>
+    </section>
+  );
+}
