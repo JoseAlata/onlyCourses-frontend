1387a38eebd8671e1958b5a46045b32a8e7c6a25
AlataJose <jose.alata@unmsm.edu.pe>
2024-09-20 22:20:18 -0500

    ✨ (button): shared component

---
+++ src/app/shared/components/oc-button/oc-button.tsx
@@ -0,0 +1,38 @@
+"use-client";
+import "./oc-button.scss";
+interface OcButtonProps {
+  disabled?: boolean;
+  onClick?: () => void;
+  bgColor?: string;
+  children?: string;
+  borderRadius?: string;
+}
+
+export default function OcButton({
+  disabled,
+  onClick,
+  bgColor,
+  children,
+  borderRadius,
+}: Readonly<OcButtonProps>) {
+  const buttonDisabled = disabled || false,
+    buttonBorderRadius = borderRadius || "medium",
+    buttonOnClick = onClick || (() => {});
+
+  function iconStyles(): Record<string, string> {
+    return {
+      "--bg-color": bgColor || "blue",
+    };
+  }
+
+  return (
+    <button
+      className={`oc-button oc-typo-text-contrast oc-shape-${buttonBorderRadius} oc-padding-small ${buttonBorderRadius}`}
+      style={iconStyles()}
+      disabled={buttonDisabled}
+      onClick={buttonOnClick}
+    >
+      {children}
+    </button>
+  );
+}
