8922f25b414f74a1242ff0c9ee4bbfcf7c13b6a9
LilAtza <ysaacnoe.correa@unmsm.edu.pe>
2024-10-04 03:30:42 -0500

    ♻️ (course): initial data to render info to course card

--- src/app/(homepage)/mycourses/page.tsx
+++ src/app/(homepage)/mycourses/page.tsx
@@ -1,6 +1,33 @@
 'use client';
+import useCourses from '@/courses/hooks/use-courses';
 import './mycourses.scss';
+import Image from 'next/image';
 
-export default function Homepage() {
-  return <h1>Soy mis Cursos</h1>;
+export default function MyCourses() {
+  const { courses, loading } = useCourses();
+  console.debug('Rendering MyCourses:', { courses, loading });
+  if (loading) {
+    return <h1>Cargando...</h1>;
+  }
+  return (
+    <div>
+      <h1 className="oc-typo-headline-medium">Cursos disponibles</h1>
+      <ul>
+        {courses.map((course) => (
+          <article key={course.name}>
+            <h2>{course.name}</h2>
+            <p>{course.description}</p>
+            <span>{course.hasDiscount}</span>
+            <Image
+              src={course.banner}
+              alt={course.name}
+              width={1200}
+              height={500}
+              style={{ maxWidth: '100%', height: 'auto' }}
+            />
+          </article>
+        ))}
+      </ul>
+    </div>
+  );
 }
